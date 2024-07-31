6178ed4268e56c7a443996f7228878cfa4871a06
Vitalii Steblovskyi <spinner1993@gmail.com>
2024-07-31 15:53:04 +0300

    Added Immutable annotations for data classes

--- app/src/main/java/com/spinner/cryptocurrencies/presentation/screen/coin_details/CoinDetailsState.kt
+++ app/src/main/java/com/spinner/cryptocurrencies/presentation/screen/coin_details/CoinDetailsState.kt
@@ -1,7 +1,9 @@
 package com.spinner.cryptocurrencies.presentation.screen.coin_details
 
+import androidx.compose.runtime.Immutable
 import com.spinner.cryptocurrencies.domain.model.CoinDetails
 
+@Immutable
 data class CoinDetailsState(
     val isLoading: Boolean = false,
     val coinDetails: CoinDetails? = null,
--- app/src/main/java/com/spinner/cryptocurrencies/presentation/screen/coin_list/CoinListState.kt
+++ app/src/main/java/com/spinner/cryptocurrencies/presentation/screen/coin_list/CoinListState.kt
@@ -1,7 +1,9 @@
 package com.spinner.cryptocurrencies.presentation.screen.coin_list
 
+import androidx.compose.runtime.Immutable
 import com.spinner.cryptocurrencies.domain.model.Coin
 
+@Immutable
 data class CoinListState(
     val isLoading: Boolean = false,
     val coins: List<Coin> = emptyList(),
